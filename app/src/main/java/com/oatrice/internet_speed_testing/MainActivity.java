0bf64ecfe896be99238285f3089244a5c3ffee33
oatpano <anirut.311@gmail.com>
2018-01-12 03:19:02 +0700

    init commit

---
+++ app/src/main/java/com/oatrice/internet_speed_testing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.oatrice.internet_speed_testing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
