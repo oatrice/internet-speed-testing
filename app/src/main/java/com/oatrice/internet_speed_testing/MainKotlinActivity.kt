8d2e3eccfae892b6a6926977cd7c450de8375932
Jacki <jackicmli@gmail.com>
2019-10-02 00:12:50 -0700

    added start button

--- app/src/main/java/com/oatrice/internet_speed_testing/MainKotlinActivity.kt
+++ app/src/main/java/com/oatrice/internet_speed_testing/MainKotlinActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Button
 import com.example.internet_speed_testing.InternetSpeedBuilder
 import com.example.internet_speed_testing.ProgressionModel
 
@@ -11,16 +12,26 @@ class MainKotlinActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var adapter: Adapter? = null
 
+    internal lateinit var startButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recyclerview)
+        startButton = findViewById(R.id.start_button)
 
         adapter = Adapter()
         recyclerView!!.layoutManager = LinearLayoutManager(this)
         recyclerView!!.adapter = adapter
 
+        startButton.setOnClickListener { view ->
+            startSpeedTest()
+        }
+    }
+
+    private fun startSpeedTest() {
         val builder = InternetSpeedBuilder(this)
         builder.setOnEventInternetSpeedListener(object : InternetSpeedBuilder.OnEventInternetSpeedListener {
             override fun onDownloadProgress(count: Int, progressModel: ProgressionModel) {
